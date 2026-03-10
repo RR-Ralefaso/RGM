9b97478de1bde2f880e0b81efcbbda351a44804c
RR-Ralefaso <rrralefaso@outlook.com>
2026-03-10 17:52:51 +0200

    gpu section works but stats arent corressponding

--- src/app.cpp
+++ src/app.cpp
@@ -191,15 +191,16 @@ int showMenu()
 #endif
 
         std::cout << COLOR_CYAN << COLOR_BOLD;
-        std::cout << "╔══════════════════════════════════════================================═╗\n";
-        std::cout << "║                         RGM  v" << VERSION << "                       ║\n";
-        std::cout << "╠══════════════════════════════════════================================═╣\n";
-        std::cout << "║                                                                       ║\n";
-        std::cout << "║  " << COLOR_GREEN  << "1.  SEND SCREEN            " << COLOR_CYAN << "║\n";
-        std::cout << "║  " << COLOR_YELLOW << "2.  RECEIVE SCREEN         " << COLOR_CYAN << "║\n";
-        std::cout << "║  " << COLOR_RED    << "0.  EXIT                   " << COLOR_CYAN << "║\n";
-        std::cout << "║                                                                       ║\n";
-        std::cout << "╚══════════════════════════════════════================================═╝\n" << COLOR_RESET;
+        std::cout << "╔════════════════════════════════╗\n";
+        std::cout << "║               RGM v" << VERSION << "       ║\n";
+        std::cout << "╠════════════════════════════════╣\n";
+        std::cout << "║                                ║\n";
+        std::cout << "║  " << COLOR_GREEN <<  "1.   SEND SCREEN              " << COLOR_CYAN << "║\n";
+        std::cout << "║  " << COLOR_YELLOW <<  "2.   RECEIVE SCREEN           " << COLOR_CYAN << "║\n";
+        std::cout << "║  " << COLOR_RED <<    "0.   EXIT                     " << COLOR_CYAN << "║\n";
+        std::cout << "║                                ║\n";
+        std::cout << "╚════════════════════════════════╝\n"
+                  << COLOR_RESET;
 
         std::cout << COLOR_BOLD << "\nEnter your choice (1-3): " << COLOR_RESET;
         std::getline(std::cin, input);
